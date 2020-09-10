488d8df41b63a613ecf22c79407b03ec56da0166
anatoly <tolitius@gmail.com>
2020-09-10 13:56:58 -0400

    dev: update examples

--- dev/dev.clj
+++ dev/dev.clj
@@ -4,14 +4,15 @@
            [cprop.core :as c]
            [next.jdbc :as jdbc]
            [next.jdbc.result-set :as rs]
-           [verter.core :as vc]
+           [verter.core :as v]
            [verter.store.postgres :as vp]
-           [yang.lang :as y]))
+           [clojure.repl :as repl]
+           [clojure.pprint :as pp]))
 
 (defn start-db []
   (println "[verter] connecting to the database..")
-  (let [{:keys [datasource queries]} (-> (c/load-config)
-                                         :verter)]
+  (let [{:keys [datasource]} (-> (c/load-config)
+                                 :verter)]
     (hk/make-datasource datasource)))
 
 (defn stop-db [datasource]
@@ -19,15 +20,20 @@
 
 (defn execute-sql! [datasource sql]
   (with-open [conn (jdbc/get-connection datasource)]
-    (jdbc/execute! conn [sql])))
+    (jdbc/execute! conn [sql]
+                   {:return-keys true
+                    :builder-fn rs/as-unqualified-lower-maps})))
 
 ; $ clojure -A:dev -A:repl
 
 ; (require 'dev)(in-ns 'dev)
 ; (def db (start-db))
-; (def v (vp/connect db))
+; (def verter (vp/connect db))
 
-; (vc/add-facts v [{:id :foo/bar/v1 :zoo 34} [{:id :foo/bar/v2 :baz 35} #inst "2019-09-09"]])
-; (vc/facts v :foo/bar/v2)
+; (v/add-facts verter [{:id :universe/one :suns 12 :planets #{:one :two :three}}
+;                      [{:id :universe/two :suns 3 :life? true} #inst "2019-09-09"]
+;                      {:id :universe/sixty-six :answer 42}])
+
+; (v/facts verter :universe/two)
 
 ; (stop-db db)
